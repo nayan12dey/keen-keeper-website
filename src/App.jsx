338dfe8e82bf3dd727f11900d5bf264533fb145f
Nayan Dey <ndeynayan123@gmail.com>
2026-04-13 22:24:45 +0530

    added tailwind css and daisyui in project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h2 className='bg-red-500'>Nayan Dey</h2>
+      <button className='btn btn-primary'>Button</button>
+    </>
+  )
+}
+
+export default App
